9050df9f19ad5c0579bf02b51cc90d645da047b9
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-06-03 04:23:43 +0000

    Add Ressource and RessourceDejaEmprunteeException classes for resource management

---
+++ src/exceptions/RessourceDejaEmprunteeException.java
@@ -0,0 +1,8 @@
+
+package exceptions;
+
+public class RessourceDejaEmprunteeException extends Exception {
+    public RessourceDejaEmprunteeException(String message) {
+        super(message);
+    }
+}
